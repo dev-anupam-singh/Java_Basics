deb21740fe8ad7a8db5f22ada74db136b9951d5e
Anupam Raj <anupamsingh1244@gmail.com>
2026-07-13 22:38:58 +0530

    Day 2: Java Variables and Input Practice

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World! Java setup done.");
+    public static void main(String[] args) { 
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
     }
 }
