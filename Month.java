962330f7448c4e443102ecde7299df709e35759d
Anupam Raj <anupamsingh1244@gmail.com>
2026-07-16 15:56:25 +0530

    Added month name program using switch case

---
+++ Month.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Month {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter month number");
+        int choice = sc.nextInt();
+
+        switch (choice){
+            case 1:
+            System.out.println("january");
+            break;
+            case 2:
+            System.out.println("febuary");
+            break ;
+            case 3:
+            System.out.println("march");
+            break;
+            case 4:
+            System.out.println("april");
+            break;
+            case 5:
+            System.out.println("may");
+            break;
+            case 6:
+            System.out.println("june");
+            break;
+            case 7:
+            System.out.println("july");
+            break;
+            case 8:
+            System.out.println("august");
+            break;
+            case 9:
+            System.out.println("september");
+            break;
+            case 10:
+            System.out.println("october");
+            break;
+            case 11:
+            System.out.println("november");
+            break;
+            case 12:
+            System.out.println("december");
+            break;
+        
+        }
+        sc.close();
+
+
+    }
+}
+    
