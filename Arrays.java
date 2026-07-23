b420456da487d41a79385c7a0397838c68015128
Anupam Raj <anupamsingh1244@gmail.com>
2026-07-23 23:53:06 +0530

    program to print marks of three subject

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Arrays {
+    public static void main(String[] args) {
+        int marks[] = new int[3];
+        marks[0] = 97; //phy
+        marks[1] = 89; //chem
+        marks[2] = 95; //math
+        //System.out.println(marks[0]);
+        //System.out.println(marks[1]);
+        //System.out.println(marks[2]);
+
+        for (int i = 0; i<3; i++){
+            System.err.println(marks[i]);
+        }
+        
+    }
+    
+}
